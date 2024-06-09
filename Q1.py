2993ee73409d334b1e08e48d87c7e8e078659781
Tanya Jain <jaintanya2006@gmail.com>
2024-06-09 09:16:42 +0530

    Python Programming Assignment 1

--- Q1.py
+++ Q1.py
@@ -1,3 +1,3 @@
 a = float(input("Enter no1: "))
 b = float(input("Enter no2: "))
-print(a+b)
+print("Sum: ", a+b)
---
+++ Q3.py
@@ -0,0 +1,5 @@
+n = int(input("Enter no: "))
+fac = 1
+for i in range(1, n+1):
+    fac = fac * i
+print("Factorial of given number: ", fac)
